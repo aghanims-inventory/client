1a55f9c20a3e707117e1ae4a795b81687ff0b141
Talha Keskin <50806631+talhakeskin@users.noreply.github.com>
2025-11-23 20:37:09 +0300

    add navbar

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './assets/styles/index.css'
 import App from './components/App/App.tsx'
+import Navbar from './components/Navbar/Navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Navbar />
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
